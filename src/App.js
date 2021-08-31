72e628b7f79d362c68669b239a5e8451024fa902
Curtisaurus <ccdrullinger@gmail.com>
2021-08-31 11:59:05 -0400

    added footer component template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header />
-      {/* Content - needs to be conditionally rendered*/}
-      {/* Footer */}
+      <Footer />
     </div>
   );
 }
