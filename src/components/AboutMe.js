afc1d3afbcb41b6db491b2df3f5c3be4eb59ac70
Curtisaurus <ccdrullinger@gmail.com>
2021-09-05 13:14:21 -0400

    added photo assets and About section text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import selfie from '../assets/images/curtis-working.jpg'
 
 export default function AboutMe() {
   return (
-    <div><h1>About</h1></div>
+    <div>
+      <img src={selfie} className="selfie" alt="Curtis Drullinger" />
+      <p>Full-stack web developer eager to apply critical thinking skills acquired during studies applied to a B.S. in Chemistry coupled with newly rooted skills in software engineering.
+          Possessing a certificate in Web Development from Michigan State University Coding Bootcamp, 
+          I am skilled in CSS, JavaScript (with ES6 standard), HTML, Node.js</p>
+      <p>An incredibly quick study with an efficient mindset, my goal is to create and streamline web-based applications while trimming the fat on any currently employed projects.  
+          I have a sharp eye for detail and excel at debugging and trouble-shooting while to produce helpful and constructive criticism of work-in-progress systems.
+          I am excited to apply my knowledge and skills to a new set of projects to increase their overall performance and propel them into their most efficient working states.</p>
+    </div>
   )
 }
