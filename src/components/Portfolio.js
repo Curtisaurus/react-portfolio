a24bf10f749c1f55d058d267e7931606a98d3cce
Curtisaurus <ccdrullinger@gmail.com>
2021-09-05 21:53:56 -0400

    added basic portfolio and project code and assets

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Project from './Project';
+import projects from '../assets/project-data/projects.json'
 
 export default function Portfolio() {
   return (
-    <div><h1>Portfolio</h1></div>
+    <div className="project-box">
+      {projects.map((project) => (
+        <Project imageUrl={project.imageUrl} name={project.name} github={project.github} livelink={project.livelink} />
+      ))}
+    </div>
   )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export default function Project() {
+export default function Project(props) {
   return (
-    <div></div>
+    <div className="project-Card">
+      <h3>{props.name}</h3>
+      <img className="project-img" src={props.imageUrl} alt={props.name}></img>
+      <a href={props.github}>GitHub</a>
+      <a href={props.livelink}>Live App</a>
+    </div>
   )
 }
