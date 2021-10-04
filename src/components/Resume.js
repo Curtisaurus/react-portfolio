aa3774ea72a9a001b1552812ce5f12631266f4f7
Curtisaurus <ccdrullinger@gmail.com>
2021-10-03 22:46:36 -0400

    updated resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ export default function Resume() {
       <h3>Proficiencies</h3>
       <p>HTML5, CSS3, JavaScript, jQuery Node.js, Express.js, MySQL, NoSQL, Sequelize, Handlebars.js, MongoDB, React, Apollo GraphQL</p>  
       <h3>Find my resume{" "}
-        <a href="https://drive.google.com/file/d/15cX9uqaEvkTxMciEohjZH5sb4DukGiQj/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/1TpCeM56XK9NPHa44RoxyunQIxhyYEMvR/view?usp=sharing">
           HERE
         </a>
       </h3>
